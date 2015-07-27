68e32cedef218c206e9bf795f7d243a8ac823017
TobyMaxham <tobias.maxham@tricept.de>
2015-07-27 20:35:24 +0200

    Changes

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \Minneola\TestFoo\Core\Application();
+$app = new \Minneola\TestFoo\Core\Application(realpath(__DIR__.'/../'));
 $app->boot();
-
 
 
 echo Cain::call();
