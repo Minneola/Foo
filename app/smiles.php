23cd6a9e6c91b619fb0612dcc8aa0f9097b3418a
TobyMaxham <tobias.maxham@tricept.de>
2016-09-01 22:37:57 +0200

    nothing changed

--- app/smiles.php
+++ app/smiles.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tmaxham
- * Date: 28.07.2015
- * Time: 02:55
+ * This file is part of the Minneola Project.
+ * Copyright (c) 2016 Tobias Maxham <git2016@maxham.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * Feel free to edit as you please, and have fun.
  */
 
 Smile::get('/', function()
@@ -11,23 +13,15 @@ Smile::get('/', function()
 	echo '<h1>It work\'s!</h1><span style="font-size: 14pt">Go to <a href="/dashboard">The Dashboard</a>.</span>';
 });
 
-
-
-
 Smile::get('/dashboard', 'SiffController@index');
 
 
 Smile::get('you', function()
 {
-
 	echo 'dfsdf';
-
 });
-
-
-
 
 Smile::post('/', function()
 {
-
+    var_dump($_POST);
 });
